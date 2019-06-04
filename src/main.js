cac26ef2c83e3c4655d9ff6ed2f9a7a647da0026
Kevin Garvey <kmgarvey279@gmail.com>
2019-06-03 20:43:46 -0700

    add css styling

--- src/main.js
+++ src/main.js
@@ -5,5 +5,14 @@ $(document).ready(function() {
     let petSpecies = $("#species").val();
     let newPet = new Tamagotchi(petName);
     newPet.setType(petSpecies);
-  })
-})
+    if (newPet.species == bulbasaur) {
+      $("#petImage").append('<img src="img/Bulbasaur.png" weight="200px" height="200px" />');
+    } else if (newPet.species == charmander) {
+      $("#petImage").append('<img src="img/Charmander.png" weight="200px" height="200px" />');
+    } else {
+      $("#petImage").append('<img src="img/Squirtle.png" weight="200px" height="200px" />');
+    }
+    $("#start").hide();
+    $("#game").show();
+  });
+});
